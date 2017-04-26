cbf24061b30ecc9cb3a30eb8bd29b1156e31a7c7
Alan Hollis <me@alanhollis.com>
2017-04-26 22:48:32 +0100

    Write region / availability back to github

--- index.js
+++ index.js
@@ -13,24 +13,73 @@ if (!process.env.AWS_EXECUTION_ENV) {
     }
 }
 
+var GitHubApi = require("github");
+
+var github = new GitHubApi({});
+github.authenticate({
+    type: "token",
+    token: process.env.github_token
+});
+
+
 exports.handler = function (event, context) {
 
     var ec2 = new AWS.EC2(config);
     ec2.describeRegions({}, function (err, data) {
         if (!err) {
-            data.Regions.map(function (region) {
-                var ec2 = new AWS.EC2(Object.assign(config, {
-                    region: region.RegionName
-                }));
-                ec2.describeAvailabilityZones({}, function (err, zones) {
-                    var zones = zones.AvailabilityZones.map(function (a) {
-                            return a.ZoneName
-                        }
-                    ).join(",");
-                    console.log(region.RegionName + "   " + zones);
 
+            let output = [];
+
+            let getOutput = (callback) => {
+                const regionLength = data.Regions.length;
+                data.Regions.map(function (region, index) {
+
+
+                    var ec2 = new AWS.EC2(Object.assign(config, {
+                        region: region.RegionName
+                    }));
+
+                    ec2.describeAvailabilityZones({}, (err, zones) => {
+
+                        var zones = zones.AvailabilityZones.map((a) => {
+                                return a.ZoneName
+                            }
+                        ).join(" , ");
+
+                        output.push("- " + region.RegionName + " | " + zones)
+
+                        if (regionLength === index + 1) {
+                            callback();
+                        }
+                    });
+
+
+                });
+            };
+
+            getOutput(() => {
+                console.log("here2");
+                github.repos.getReadme({
+                    owner: 'alan01252',
+                    repo: 'aws-regions-and-availability-zone-list',
+                }).then(function (readme) {
+
+                    console.log(output);
+                    github.repos.updateFile({
+                        owner: 'alan01252',
+                        repo: 'aws-regions-and-availability-zone-list',
+                        path: 'README.md',
+                        sha: readme.data.sha,
+                        message: 'Updating regions and availability zones',
+                        content: new Buffer(output.join("\r\n")).toString('base64')
+                    }).then(function (result) {
+                        console.log(result);
+                    }).catch(function (err) {
+                        console.log(err)
+                    });
                 })
-            })
+            });
+
         } else {
             console.log(err);
         }
